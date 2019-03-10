2649fb315328c0bf7d5095387a557b93c9cff781
peleg122 <peleg122@gmail.com>
2019-03-10 22:54:36 +0200

    Update BasicCheck.sh

--- BasicCheck.sh
+++ BasicCheck.sh
@@ -9,12 +9,12 @@ if [ "$goodmake" -ne "0" ] ; then
         echo -e "\t\t$(tput setaf 7)Pass/$(tput setaf 1)Fail\t $(tput setaf 7)Pass/$(tput setaf 1)Fail\t  $(tput setaf 7)Pass/$(tput setaf 1)Fail\n"
         exit 7
 fi
-valgrind --tool=memcheck ${@:3} --leak-check=full --error-exitcode=1 -q  ./$program &> /dev/null
+valgrind --tool=memcheck ${@:3}  --leak-check=full --error-exitcode=1 -q  ./$program &> /dev/null
 memcheck=$?
 valgrind --tool=helgrind -q --error-exitcode=1 ./$program &> /dev/null
 racecheck=$?
 final=$goodmake$memcheck$racecheck
-#cd - &> /dev/null
+cd - &> /dev/null
 if [ "$final" == "000" ] ; then
         echo -e "\n\t\tCompilation: \tMemory leak: \t Race check:\n"
         echo -e "\t\t $(tput setaf 2)Pass/$(tput setaf 7)Fail\t $(tput setaf 2)Pass/$(tput setaf 7)Fail\t  $(tput setaf 2)Pass/$(tput setaf 7)Fail\n"
