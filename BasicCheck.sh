b752d474dd93d847a9def0fb1087e521098a552a
peleg122 <peleg122@gmail.com>
2019-03-10 21:28:58 +0200

    Update BasicCheck.sh

--- BasicCheck.sh
+++ BasicCheck.sh
@@ -19,19 +19,19 @@ if [ "$final" == "000" ] ; then
         echo -e "\n\t\tCompilation: \tMemory leak: \t Race check:\n"
         echo -e "\t\t $(tput setaf 2)Pass/$(tput setaf 7)Fail\t $(tput setaf 2)Pass/$(tput setaf 7)Fail\t  $(tput setaf 2)Pass/$(tput setaf 7)Fail\n"
         exit 0
-elif [ "$final" == "010" ] ; then
+fi
+if [ "$final" == "010" ] ; then
         echo -e "\n\t\tCompilation: \tMemory leak: \t Race check: \n"
         echo -e "\t\t$(tput setaf 2)Pass/$(tput setaf 7)Fail\t $(tput setaf 7)Pass/$(tput setaf 1)Fail\t  $(tput setaf 2)Pass/$(tput setaf 7)Fail\n"
         exit 2
-elif [ "$final" == "001" ] ; then
+fi
+if [ "$final" == "001" ] ; then
         echo -e "\n\t\tCompilation: \tMemory leak: \t Race check: \n"
         echo -e "\t\t$(tput setaf 2)Pass/$(tput setaf 7)Fail\t $(tput setaf 2)Pass/$(tput setaf 7)Fail\t  $(tput setaf 7)Pass/$(tput setaf 1)Fail\n"
         exit 1
-elif [ "$final" == "011" ] ; then
+fi
+if [ "$final" == "011" ] ; then
         echo -e "\n\t\tCompilation: \tMemory leak: \t Race check: \n"
         echo -e "\t\t$(tput setaf 2)Pass/$(tput setaf 7)Fail\t $(tput setaf 7)Pass/$(tput setaf 1)Fail\t  $(tput setaf 7)Pass/$(tput setaf 1)Fail\n"
         exit 3
-else
-        echo -e "\nCode Is Wrong!"
-        exit 8
 fi
